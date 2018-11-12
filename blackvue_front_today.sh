b5ba63b1b2818a0d7630453c3d1a7cf26355b4a6
Minyoung An <minyoung.an@gmail.com>
2018-11-12 18:14:35 -0500

    Init commit

---
+++ blackvue_front_today.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Init variables
+DASHCAM_STORAGE="/mnt/PASSPORT/BlackVue-Front"
+PREFIX="/Record/"
+
+# IP address
+DASHCAM_IP=192.168.8.120
+
+export TODAY=`date +%Y%m%d`
+
+# 192.168.8.120
+echo `date '+%Y-%m-%d %H:%M:%S'`_blackvue_front 
+cd "$DASHCAM_STORAGE"
+
+for file in `curl http://$DASHCAM_IP/blackvue_vod.cgi | sed 's/^n://' | sed 's/F.mp4//' | sed 's/R.mp4//' | sed 's/,s:1000000//' | sed $'s/\r//' | grep $TODAY`; do
+	# Set VideoName
+	VIDEONAME=${file#$PREFIX}
+	
+	# Front Video
+	wget -c http://$DASHCAM_IP$file\F.mp4;
+	if [ -f $VIDEONAME\F.mp4 ]; then
+		sqlite3 /mnt/PASSPORT/tesla.db  "INSERT OR IGNORE INTO videos(VideoName,CameraSide) VALUES('$VIDEONAME','F');"
+	fi
+
+	# Left Video
+	wget -c http://$DASHCAM_IP$file\R.mp4;
+	if [ -f $VIDEONAME\R.mp4 ]; then
+		sqlite3 /mnt/PASSPORT/tesla.db  "INSERT OR IGNORE INTO videos(VideoName,CameraSide) VALUES('$VIDEONAME','L');"
+	fi
+
+	wget -nc http://$DASHCAM_IP$file\F.thm;
+	wget -nc http://$DASHCAM_IP$file\R.thm;
+	wget -nc http://$DASHCAM_IP$file.gps;
+	wget -nc http://$DASHCAM_IP$file.3gf;
+done
---
+++ blackvue_youtube_upload.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Init Variables
+# File Location
+dashcam_storage_front="/mnt/PASSPORT/BlackVue-Front"
+dashcam_storage_back="/mnt/PASSPORT/BlackVue-Back"
+
+# Youtube
+youtube_description="Tesla, BlackVue"
+
+# Date
+today_datetime=$(date '+%Y-%m-%d %H:%M:%S')
+
+
+# Get the latest event video
+video=$(sqlite3 /mnt/PASSPORT/tesla.db -cmd ".timeout 5000" "SELECT VideoName,CameraSide FROM Videos AS v WHERE VideoName LIKE strftime('%Y%m%d') || '%E' AND NOT EXISTS (SELECT 1 FROM uploads WHERE VideoName = v.VideoName AND CameraSide = v.CameraSide) ORDER BY VIDEONAME DESC LIMIT 1;")
+video_mode="Event Mode"
+
+if [[ -n "${video// /}" ]]; then
+	# Event video found
+	true
+else
+	# Event video not found
+	video=$(sqlite3 /mnt/PASSPORT/tesla.db -cmd ".timeout 5000" "SELECT VideoName,CameraSide FROM Videos AS v WHERE VideoName LIKE strftime('%Y%m%d') || '%P' AND NOT EXISTS (SELECT 1 FROM uploads WHERE VideoName = v.VideoName AND CameraSide = v.CameraSide) ORDER BY VIDEONAME DESC LIMIT 1;")
+	video_mode="Parking Mode"
+	if [[ -n "${video// /}" ]]; then
+		# Parking video found
+		true
+	else
+		# Parking video not found
+		echo "$today_datetime-Warning:No video to upload"
+		exit
+	fi
+fi
+
+# Get video file name
+video_name=$(echo "$video" | head -c 17)
+camera_side=$(echo "$video" | tail -c 2)
+
+
+# Function
+youtube_upload()
+{
+	youtube_id=$(youtube-upload --privacy "private" --title\="$1" --playlist\="BlavckVue" --description\="$youtube_description, $video_mode" --tags\="$youtube_description, $video_mode" "$1")
+	if [[ -n "${youtube_id// /}" ]]; then
+		sqlite3 /mnt/PASSPORT/tesla.db -cmd ".timeout 5000" "INSERT INTO uploads (VideoName,CameraSide,YoutubeID) VALUES ('$video_name','$camera_side','$youtube_id');"
+		echo "$today_datetime-Success:$video|$youtube_id"
+	else
+		echo "$today_datetime-Error:$video|$youtube_id|No youtube_id returned"
+	fi
+}
+
+# Check camera side
+if [ $camera_side == "F" ] 
+then
+	file=$dashcam_storage_front/$video_name\F.mp4
+	youtube_upload $file	
+elif [ $camera_side == "L" ] 
+then
+	file=$dashcam_storage_front/$video_name\R.mp4
+	youtube_upload $file	
+elif [ $camera_side == "B" ] 
+then
+	file=$dashcam_storage_back/$video_name\F.mp4
+	youtube_upload $file	
+elif [ $camera_side == "R" ] 
+then
+	file=$dashcam_storage_back/$video_name\R.mp4
+	youtube_upload $file	
+else
+	echo "$today_datetime-Error:$video|$youtube_id|Wrong CameraSide"
+fi
